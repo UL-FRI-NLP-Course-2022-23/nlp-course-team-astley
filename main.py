dcf61f810f721d7fc1c45f4964b3176d8642b857
Matjaž Ciglič <matjaz.ciglic.jst@gmail.com>
2023-04-28 21:51:15 +0200

    Deleted unnecesarry code

--- main.py
+++ main.py
@@ -2,57 +2,10 @@ import torch
 import os
 from models import StoryEndingGenerator, SentimentAnalysis
 
-from torch.utils.data import Dataset, DataLoader
-from transformers import BertTokenizer, BertForSequenceClassification, BertForTokenClassification
-from transformers import GPT2Tokenizer, GPT2LMHeadModel
-from transformers import TextDataset, DataCollatorForLanguageModeling
-from transformers import Trainer, TrainingArguments
 
 
 os.environ["KMP_DUPLICATE_LIB_OK"] = "True"
 
-# reads the entire file into one string
-def read_file_as_string(filename):
-    with open(filename, 'r') as file:
-        return file.read()
-
-# Roughly divides a given string into a list of sentences (strings)
-# could be improved
-def sentence_extraction(text):
-    tmp = text.replace('.', '<SE>')
-    tmp = tmp.replace('?', '<SE>')
-    tmp = tmp.replace('!', '<SE>')
-    return tmp.split('<SE>')
-
-
-# Extracts sentiment from a list of sentences based on SentimentAnalysis sa
-def extract_sentiment(sa, sentences):
-    sentiment_matrix = {}
-
-    for sentence in sentences:
-        # sentiment from sentence
-        sentiment = sentiment_analysis.predict_sentiment(sentence)
-
-        # TODO: get named entities from sentence
-        # print(sentiment_analysis.predict_named_entities(sentence))
-        entities = ["Michael", "Rachel"]
-
-        # add to resulting array (dict)
-            # entiments per person
-            # sentiments per pairs of persons
-        for e1 in entities:
-            for e2 in entities:
-                if e1 is e2:
-                    if e1 not in sentiment_matrix:
-                        sentiment_matrix[e1] = [0, 0, 0]
-                    sentiment_matrix[e1][sentiment] += 1
-                else:
-                    if (e1, e2) not in sentiment_matrix:
-                        sentiment_matrix[(e1, e2)] = [0, 0, 0]
-                    sentiment_matrix[(e1, e2)][sentiment] += 1
-
-    # return matrix
-    return sentiment_matrix
 
 
 # Main
@@ -63,19 +16,6 @@ if __name__ == '__main__':
     # train_sentiment_model(None)
     sentiment_analysis = SentimentAnalysis()
     sentiment_analysis.read_stories_get_sentiment("./fairy_tales")
-
-    # read a file
-    # TODO: story = read_file_as_string('./zgodbice/1.txt')
-    story = "Michael likes Rachel. Michael is a bad man. Rachel is great. Test"
-    # separate to sentences
-    sentences = sentence_extraction(story)
-
-    data = extract_sentiment(sentiment_analysis, sentences)
-    # display data
-    print(data)
-
-    # print(sentiment_analysis.predict_sentiment(["I like you.", "I love you."]))
-    # print(sentiment_analysis.do_something(["Michael ate an apple. Michael is fat."]))
     
     # basic gpt2
     ending_model = StoryEndingGenerator()
